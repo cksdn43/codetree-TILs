5e77e89c8850d47940bfcf146191dec26b158dc9
Cha_nu <101384306+cksdn43@users.noreply.github.com>
2024-03-16 19:46:43 +0900

    Title: 진수 to 진수, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240316/진수 to 진수/transformation-of-number-system.py
+++ 240316/진수 to 진수/transformation-of-number-system.py
@@ -4,15 +4,18 @@ n = list(input())
 L = len(n)
 result = 0
 
+# a진수로 변환
 for i in range(L):
     result += int(n[-i-1]) * a**i
 
 # 리스트재활용
 n.clear()
+
+# b진수로 변환
 while result >= 1:
     n.append(result % b)
     result //= b
 
-# 이진수 출력
+# 결과 출력
 for i in n[::-1]:
     print(i, end='')
