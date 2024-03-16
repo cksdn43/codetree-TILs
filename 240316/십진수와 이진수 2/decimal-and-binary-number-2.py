211b1167a8f77f5a0b0cf4eb6db708e6305c85ca
Cha_nu <101384306+cksdn43@users.noreply.github.com>
2024-03-16 19:43:55 +0900

    Title: 십진수와 이진수 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240316/십진수와 이진수 2/decimal-and-binary-number-2.py
+++ 240316/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -1,16 +1,18 @@
+# 변수 선언 및 입력
 N = list(input())
 L = len(N)
 result = 0
 
 for i in range(L):
     result += int(N[-i-1]) * 2**i
-
+# 십진수변환에 17곱
 result *= 17
+# 리스트재활용
 N.clear()
 while result >= 1:
     N.append(result % 2)
     result //= 2
-N.reverse()
 
-for i in N:
+# 이진수 출력
+for i in N[::-1]:
     print(i, end='')
