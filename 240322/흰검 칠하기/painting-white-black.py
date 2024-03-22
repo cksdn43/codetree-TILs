bb3c3fa39f1bffea09b6c8b826182c4c722c4bcc
Cha_nu <101384306+cksdn43@users.noreply.github.com>
2024-03-22 14:12:53 +0900

    Title: 흰검 칠하기, Time: 174ms, Memory: 26MB, Status: Passed - CodeTree

--- 240322/흰검 칠하기/painting-white-black.py
+++ 240322/흰검 칠하기/painting-white-black.py
@@ -5,30 +5,35 @@ n = int(input())
 tile = {0: [0, 0, -1]}
 cur_location, white, black, gray = 0, 0, 0, 0
 
+
 # 타일 칠하기
 # Black: 0 White: 1 Gray: 2
 for _ in range(n):
     # 명령어 입력 및 처리
     move, direction = input().split()
-    move = int(move)
     if direction == 'R':
         color, step = 0, 1
     else:
         color, step = 1, -1
     
-    for i in range(move):
+    for i in range(1,int(move)):
         # 타일 색칠
         if tile[cur_location][2] != 2:
             tile[cur_location][2] = color
             tile[cur_location][color] += 1
             if tile[cur_location][0] >= 2 and tile[cur_location][1] >= 2:
                 tile[cur_location][2] = 2
-        # 마지막 타일 비도착
-        if i != (move - 1):
-            cur_location += step
-            # 처음 도착한 타일 초기화
-            if cur_location not in tile:
-                tile[cur_location] = [0, 0, -1]
+        cur_location += step
+        # 처음 도착한 타일 초기화
+        if cur_location not in tile:
+            tile[cur_location] = [0, 0, -1]
+
+    # 도착한 타일 색칠
+    if tile[cur_location][2] != 2:
+        tile[cur_location][2] = color
+        tile[cur_location][color] += 1
+        if tile[cur_location][0] >= 2 and tile[cur_location][1] >= 2:
+            tile[cur_location][2] = 2
 
 # 타일 색 계산
 for key in tile.keys(): # T(k)
