a66952fb84764a6b849396611c8117562176484b
Cha_nu <101384306+cksdn43@users.noreply.github.com>
2024-03-22 13:44:07 +0900

    Title: 흰검 칠하기, Time: 177ms, Memory: 26MB, Status: Passed - CodeTree

--- 240322/흰검 칠하기/painting-white-black.py
+++ 240322/흰검 칠하기/painting-white-black.py
@@ -1,31 +1,49 @@
 # 변수 선언 및 입력
 n = int(input())
 
-tile = {0: []}
+tile = {0: [0, 0, -1]}
 cur_location, white, black, gray = 0, 0, 0, 0
 
+
 # 타일 칠하기
-# Black: 0 white: 1 {location: [paint_black, paint_white, last_color] }
+# Black: 0 White: 1 Gray: 2
+# {location: [paint_black_count, paint_white_count, last_color] }
 for _ in range(n): # T(n)
+    # 명령어 입력 및 처리
     move, direct = input().split()
     if direct == 'R':
-        value = 0
-        step = 1
+        color, step = 0, 1
     else:
-        value = 1
-        step = -1
-    for i in range(1,int(move)): # T(x)
-        tile.setdefault(cur_location, []).append(value)
+        color, step = 1, -1
+    
+    for _ in range(1,int(move)): # T(x)
+        # 처음 도착한 타일 초기화
+        if cur_location not in tile:
+            tile[cur_location] = [0, 0, -1]
+        # 타일 색칠
+        if tile[cur_location][2] != 2:
+            tile[cur_location][2] = color
+            tile[cur_location][color] += 1
+        if tile[cur_location][0] >= 2 and tile[cur_location][1] >= 2:
+            tile[cur_location][2] = 2
         cur_location += step
-    tile.setdefault(cur_location, []).append(value)
+
+    # 도착한 타일 색칠  
+    if cur_location not in tile:
+        tile[cur_location] = [0, 0, -1]
+    if tile[cur_location][2] != 2:
+            tile[cur_location][2] = color
+            tile[cur_location][color] += 1
+    if tile[cur_location][0] >= 2 and tile[cur_location][1] >= 2:
+            tile[cur_location][2] = 2
 
 # 타일 색 계산
 for key in tile.keys(): # T(k)
-    if tile[key].count(0) >= 2 and tile[key].count(1) >= 2:
+    if tile[key][2] == 2:
         gray += 1
-    elif tile[key][-1] == 0:
+    elif tile[key][2] == 0:
         black += 1
     else:
         white += 1
 
-print("{} {} {}".format(white, black, gray))
+print(white, black, gray)
