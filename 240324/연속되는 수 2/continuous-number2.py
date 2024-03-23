66ca6b3432d9a85e9261736a4aca3f82d4641a73
Cha_nu <101384306+cksdn43@users.noreply.github.com>
2024-03-24 00:02:44 +0900

    Title: 연속되는 수 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240324/연속되는 수 2/continuous-number2.py
+++ 240324/연속되는 수 2/continuous-number2.py
@@ -5,14 +5,15 @@ max_cnt = 1
 N = int(input())
 cur_value = int(input())
 
+# 최장 연속 부분 수열 계산
 for i in range(1,N):
     input_value = int(input())
     if cur_value == input_value:
         cnt += 1
     else:
         if cnt > max_cnt:
-            max_cnt == cnt
-        cnt = 0
+            max_cnt = cnt
+        cnt = 1
         cur_value = input_value
 
 print(max_cnt)
