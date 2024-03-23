8ed1874a40c2c28d6c437857cc9f5cd400596ada
Cha_nu <101384306+cksdn43@users.noreply.github.com>
2024-03-23 20:58:25 +0900

    Title: 계속 중첩되는 사각형, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240323/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ 240323/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -8,13 +8,17 @@ cur_color, result = 1, 0
 # red: 0, blue: 1
 for _ in range(n):
     x1, y1, x2, y2 = map(int, input().split())
+    # y1좌표를 인덱스에 맞춰줍니다.
     y1 += 100
     y2 += 100
+    # 박스색칠
     for i in range(x1, x2):
         for j in range(y1, y2):
             box[i][j] = cur_color
+    # 칠해지는 박스 색 결정
     cur_color = (cur_color + 1) % 2
 
+# 파란박스 넓이계산 후 출력
 for k in box.values():
     result += k.count(1)
 print(result)
