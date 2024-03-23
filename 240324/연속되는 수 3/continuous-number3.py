03fb778a2951374a191900ac082f62fd72a4bcb6
Cha_nu <101384306+cksdn43@users.noreply.github.com>
2024-03-24 01:37:36 +0900

    Title: 연속되는 수 3, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240324/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,21 @@
+# 변수 선언 및 입력
+key, cnt = -1, 1
+max_cnt = 1
+
+
+N = int(input())
+
+# 최장 연속 부분 수열 계산
+for i in range(N):
+    input_value = int(input())
+    if (input_value < 0 and key < 0) or (input_value > 0 and key > 0):
+        cnt += 1
+    else:
+        if cnt > max_cnt:
+            max_cnt = cnt
+        cnt = 1
+        key *= -1
+if cnt > max_cnt:
+    max_cnt = cnt
+
+print(max_cnt)
