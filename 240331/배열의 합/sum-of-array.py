137e7ca863db385b76e6d3b323977308a8c7463e
Cha_nu <101384306+cksdn43@users.noreply.github.com>
2024-03-31 00:48:01 +0900

    Title: 배열의 합, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240331/배열의 합/sum-of-array.py
+++ 240331/배열의 합/sum-of-array.py
@@ -1,5 +1,5 @@
 import sys
-arr = [list(map(int,sys.stdin.readline().split())) for _ in range(4)]
+arr = [print(sum(list(map(int,sys.stdin.readline().split())))) for _ in range(4)]
 
-for i in range(4):
-    print(sum(arr[i]))
+# for i in range(4):
+#     print(sum(arr[i]))
